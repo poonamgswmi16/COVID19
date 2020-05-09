d924abce6482023877a2fe7f395951a681045d9e
poonamgswmi16 <poonamgswmi16@gmail.com>
2020-05-09 18:15:18 +0530

    Updates

---
+++ precache-manifest.9e4496a76d2f2de753f3d087d9c8f140.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dadc562abe88b0474f7f62209c74b928",
+    "url": "/COVID19/static/media/covid.dadc562a.svg"
+  },
+  {
+    "revision": "cd78e30ff2f517c5d1d3",
+    "url": "/COVID19/static/css/main.82d5793e.chunk.css"
+  },
+  {
+    "revision": "98f525fc9f64caa21f0d",
+    "url": "/COVID19/static/js/runtime~main.88308df9.js"
+  },
+  {
+    "revision": "806d5540a84f4b4b42cfb94f8d18a02e",
+    "url": "/COVID19/static/media/safety.806d5540.jpg"
+  },
+  {
+    "revision": "ee05e43e80a8b629cd90",
+    "url": "/COVID19/static/js/2.84d86dab.chunk.js"
+  },
+  {
+    "revision": "cd78e30ff2f517c5d1d3",
+    "url": "/COVID19/static/js/main.36e737f2.chunk.js"
+  },
+  {
+    "revision": "1bcbd0bd190d5d416093f3f541db3e50",
+    "url": "/COVID19/static/media/fight.1bcbd0bd.png"
+  },
+  {
+    "revision": "dde66942be6875c50ca0d114ef7fd065",
+    "url": "/COVID19/static/media/greet.dde66942.png"
+  },
+  {
+    "revision": "cb64eb7d55a1d79e80bf6f419d674c58",
+    "url": "/COVID19/static/media/handshaking.cb64eb7d.png"
+  },
+  {
+    "revision": "ee05e43e80a8b629cd90",
+    "url": "/COVID19/static/css/2.9daf9c49.chunk.css"
+  },
+  {
+    "revision": "f0721cebe2ff0e5154d2fce554a06951",
+    "url": "/COVID19/index.html"
+  }
+];
