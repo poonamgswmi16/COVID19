b05a1b4b3811f9843e3e234a9ee6a395814187c2
poonamgswmi16 <poonamgswmi16@gmail.com>
2020-05-09 12:06:44 +0530

    Updates

---
+++ precache-manifest.0818d70877a29d61bb34994d0caf2b12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dadc562abe88b0474f7f62209c74b928",
+    "url": "/COVID19/static/media/covid.dadc562a.svg"
+  },
+  {
+    "revision": "98f525fc9f64caa21f0d",
+    "url": "/COVID19/static/js/runtime~main.88308df9.js"
+  },
+  {
+    "revision": "a6f89597df69c224801b",
+    "url": "/COVID19/static/js/main.76ce7c7a.chunk.js"
+  },
+  {
+    "revision": "f1752d9bf70639035359",
+    "url": "/COVID19/static/js/2.812e0694.chunk.js"
+  },
+  {
+    "revision": "a6f89597df69c224801b",
+    "url": "/COVID19/static/css/main.82d5793e.chunk.css"
+  },
+  {
+    "revision": "f1752d9bf70639035359",
+    "url": "/COVID19/static/css/2.9daf9c49.chunk.css"
+  },
+  {
+    "revision": "a402c7716b4d47a960bf7e40244c85bf",
+    "url": "/COVID19/index.html"
+  }
+];
